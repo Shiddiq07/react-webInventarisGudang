b4618ec96cbd9b2e26312aca7633ce85f7de11a8
muhammadshiddiq <mshiddiqs13@gmailcom>
2025-09-23 11:41:01 +0700

    9. fix searchParams at laporan toPDF

--- src/app/(auth)/admin/laporan/toPdf/page.tsx
+++ src/app/(auth)/admin/laporan/toPdf/page.tsx
@@ -50,6 +50,10 @@ type GroupedTableData = {
 
 
 export default function StockOpnameTable () {
+    if (useSearchParams() !== null) {
+          const searchParams = useSearchParams();
+
+    }
   const searchParams = useSearchParams();
   const bulan = searchParams?.get('bulan');
   const tahun = searchParams?.get('tahun');
